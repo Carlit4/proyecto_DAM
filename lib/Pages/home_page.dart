a8ce317077805fb5ae8a6abdd636adacbb182637
carlit4 <cpujadas@usm.cl>
2024-11-14 22:58:29 -0300

    config

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/icon_map.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.restaurant_menu),
+            Text('COOKLY', style: TextStyle(fontWeight: FontWeight.bold),
+          ],
+        ),
+      ),
+    );
+  }
+}
